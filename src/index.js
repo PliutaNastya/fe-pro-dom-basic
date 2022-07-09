5b4cf7c1dcc924b138d0fba482a703614fd14279
Anastasiia <koicheva1996@gmail.com>
2022-07-09 14:45:56 +0300

    added homework (basic-dom)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  return document
+    .querySelectorAll('.item')
+    .forEach((elem, index) =>
+      index % 2 ? elem : (elem.style.backgroundColor = 'red')
+    );
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+  for (let elem of document.getElementsByTagName('li')) {
+    if (elem.matches('.likedItem')) {
+      return (elem.style.backgroundColor = 'blue');
+    }
+  }
+};
